d8ce83c54e061eeb9026b6926c885ed369ee6167
Hasan Sabri <hsabri2007@gmail.com>
2026-04-03 17:25:45 +0100

    nice

--- src/main.cxx
+++ src/main.cxx
@@ -1,10 +1,43 @@
 #include "Penguine.hxx"
+#include "mathematics/Vec.hxx"
+#include "rendering/Mesh.hxx"
+#include <vector>
 
 class SoulsLike : public Penguine::Application
 {
   public:
-	SoulsLike() : Penguine::Application({{{1280, 720}}, false, "Hello"})
+	Penguine::Rendering::Shader shader;
+	Penguine::Rendering::Mesh	mesh;
+	SoulsLike()
+		: Penguine::Application({
+			  {
+				  {
+					  1280,
+					  720,
+				  },
+			  },
+			  false,
+			  "Hello",
+		  }),
+		  shader("../assets/shaders/Basic.vert", "../assets/shaders/Basic.frag"),
+		  mesh(
+			  {
+				  {{-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+				  {{0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+				  {{0.0f, 0.5f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.5f, 1.0f}},
+			  },
+			  {
+				  {{0, 1, 2}},
+			  })
 	{
+	}
+
+	void Update(float deltaTime) override
+	{
+		shader.Use();
+		shader.SetVec("ourColor", Penguine::Math::Vec<float, 3>({1.0f, 0.5f, 0.2f}));
+
+		mesh.Draw(shader);
 	}
 };
 
